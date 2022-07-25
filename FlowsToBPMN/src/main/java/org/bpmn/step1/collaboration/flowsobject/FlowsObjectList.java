3a4e8e35be0aab6ccd4e84c057adbb2ab3dc7eef
dportgaz <SodaFantakrotons1!>
2022-07-25 19:57:15 +0200

    feat(FlowsToBPMN): Reorganised directories by adding steps form concept

---
+++ FlowsToBPMN/src/main/java/org/bpmn/step1/collaboration/flowsobject/FlowsObjectList.java
@@ -0,0 +1,22 @@
+package org.bpmn.step1.collaboration.flowsobject;
+
+import java.util.ArrayList;
+
+public class FlowsObjectList extends AbstractFlowsObject {
+
+	public ArrayList<AbstractFlowsObject> DataModelActionLogs;
+
+	@Override
+	public String toString() {
+
+		String retString = "";
+		retString += DataModelActionLogs;
+
+		return retString;
+	}
+
+	public ArrayList<AbstractFlowsObject> getList() {
+		return this.DataModelActionLogs;
+	}
+
+}
