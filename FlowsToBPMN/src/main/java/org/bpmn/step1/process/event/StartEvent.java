1dbe619a57710a584a35e71e5aba81ca13f9fca2
Marko <pejic-marko@gmx.de>
2022-07-28 23:33:55 +0200

    feature(FillProcesses): Added StartEvent SequenceFlow to BPMN XML

--- FlowsToBPMN/src/main/java/org/bpmn/step1/process/event/StartEvent.java
+++ FlowsToBPMN/src/main/java/org/bpmn/step1/process/event/StartEvent.java
@@ -6,13 +6,20 @@ import org.bpmn.step1.process.flow.SequenceFlow;
 public class StartEvent {
 
     String id;
+    Double createdEntityId;
     SequenceFlow outgoing;
     public StartEvent() {
         this.id = RandomIdGenerator.generateRandomUniqueId(6);
-
     }
     public String getId(){
         return this.id;
     }
 
+    public void setCreatedEntityId(Double createdEntityId) {
+        this.createdEntityId = createdEntityId;
+    }
+
+    public Double getCreatedEntityId() {
+        return createdEntityId;
+    }
 }
